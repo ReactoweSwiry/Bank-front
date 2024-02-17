c536c8ea28a1e8e2d22bb94ccf9507cb3cdc8e95
Matt5246 <116891485+Matt5246@users.noreply.github.com>
2024-02-17 18:57:31 +0100

    some good improvements
    better color and more responsive

--- my-app/src/app/home/page.tsx
+++ my-app/src/app/home/page.tsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button'
 export default function Home() {
     return (
         <header className='size-full'>
-            <h1 className="text-4xl font-bold">Bank App</h1>
+            <h1 className="text-4xl font-bold mt-8">Bank App</h1>
             <ModeToggle />
         </header>
     );
