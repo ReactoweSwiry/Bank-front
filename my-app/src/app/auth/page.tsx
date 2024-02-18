d9043153cf932979e626d8c12562b4d221090998
kayYZ1 <overek7133@gmail.com>
2024-02-18 21:58:37 +0100

    feat(auth): h2 animation

--- my-app/src/app/auth/page.tsx
+++ my-app/src/app/auth/page.tsx
@@ -1,11 +1,14 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
-import SignIn  from './sign-in';
+import { ModeToggle } from "@/components/toggleTheme";
+
+import SignIn from './sign-in';
 import SignUp from './sign-up';
 
 const AuthPage = () => {
 	return (
 		<div className="flex flex-row h-screen">
+			<ModeToggle />
 			<div className="w-full md:w-3/5 flex flex-col justify-center items-center">
 				<Tabs defaultValue="signin" className="w-[400px]">
 					<TabsList className="grid w-full grid-cols-2">
@@ -20,8 +23,11 @@ const AuthPage = () => {
 					</TabsContent>
 				</Tabs>
 			</div>
-			<div className="hidden md:block md:w-3/5 bg-gray-200">
-				<h2>Bank opole</h2>
+			<div className="hidden md:flex md:w-3/5 bg-gray-200 justify-center items-center">
+				<h2 className="animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 
+					bg-clip-text text-transparent text-5xl font-black">
+					Bank Opole
+				</h2>
 			</div>
 		</div>
 	)
