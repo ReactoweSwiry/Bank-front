9b48fd74224c7243c528134da61743f0a5f78911
kayYZ1 <overek7133@gmail.com>
2024-02-19 09:29:35 +0100

    feat: animated image

--- my-app/src/app/auth/page.tsx
+++ my-app/src/app/auth/page.tsx
@@ -2,8 +2,11 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 import { ModeToggle } from "@/components/toggleTheme";
 
+import opoleAnimated from "@/assets/opoleAnimated.png"
+
 import SignIn from './sign-in';
 import SignUp from './sign-up';
+import Image from "next/image";
 
 const AuthPage = () => {
 	return (
@@ -23,11 +26,12 @@ const AuthPage = () => {
 					</TabsContent>
 				</Tabs>
 			</div>
-			<div className="hidden md:flex md:w-3/5 bg-gray-200 justify-center items-center">
-				<h2 className="animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 
+			<div className='hidden md:flex md:w-3/5 bg-gray-200 justify-center items-center'>
+				{/*<h2 className="animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 
 					bg-clip-text text-transparent text-5xl font-black">
 					Bank Opole
-				</h2>
+				</h2>*/}
+				<Image src={opoleAnimated} alt="Opole animated logo" className="w-full h-full object-cover"/>
 			</div>
 		</div>
 	)
